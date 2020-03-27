d88f3500c298fb8b7b23d5fe93a472994261f83b
Russell Churchwell <russellchurchwell@russells-mbp.attlocal.net>
2020-03-27 01:06:04 -0500

    add initial program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Linq;
+
+namespace ChurchwellRussell_SprintApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool menuRun = true;
+            Utilities util = new Utilities;
+
+            util.PrintMenu();
+            util.GetKey();
+        }
+    }
+}
