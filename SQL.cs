d9bf6b0359b228ce520315a881eecae220c86eb9
Russell Churchwell <russellchurchwell@russells-mbp.attlocal.net>
2020-03-27 19:31:03 -0500

    SQL class to handle SQL connections.

---
+++ SQL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace ChurchwellRussell_TimeTrackerApp
+{
+    public class SQL
+    {
+        MySqlConnection conn;
+        string Sconn = "server=127.0.0.1; user = root; database = RussellChurchwell_MDV229_Database_Month(202003); port = 8889; password = root";
+        Utilities util =new Utilities();
+
+
+        public SQL()
+        {
+        }
+
+        public MySqlConnection Connect()
+        {
+            // Connection object
+            conn = new MySqlConnection(Sconn);
+
+            try
+            {
+                // Try to open connection
+                conn.Open();
+
+                // Clear and write if connected
+                Console.Clear();
+                Console.WriteLine("Connection opened.");
+
+                // Get key press
+                util.GetKey();
+            }
+            catch (MySqlException e)
+            {
+                // Print error
+                Console.WriteLine(e.ToString());
+            }
+
+            return conn;
+        }
+    }
+}
